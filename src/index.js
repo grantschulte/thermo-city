352bf74e78f9af944f1053d867ee2104aeb1d4e5
Grant Schulte <schulte.grant@gmail.com>
2017-08-12 16:18:45 -0400

    Adds main app.scss stylesheet and imports

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 "use strict";
 
+// Include stylesheets here. Import new scss stylesheets into app.scss
+// if you wish to keep this file clean.
+
 require("ace-css/css/ace.css");
+require("./styles/app.scss");
 
 // Require index.html so it gets copied to dist
+
 require("./index.html");
+
+// Elm initialization
 
 var Elm = require("./Main.elm");
 var mountNode = document.getElementById("main");
-
-// .embed() can take an optional second argument. This would be an object
-// describing the data we need to start a program, i.e. a
-// userID or some token.
 
 var app = Elm.Main.embed(mountNode, {
   nodeEnv: NODE_ENV,
