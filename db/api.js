d8d8a2d11e9de034cca074dd130906646a38e2ec
Grant Schulte <schulte.grant@gmail.com>
2017-08-13 08:39:14 -0400

    Resolves db.json path

--- db/api.js
+++ db/api.js
@@ -1,4 +1,5 @@
 var jsonServer = require("json-server");
+var path = require("path");
 
 // Returns an Express server
 var server = jsonServer.create();
@@ -6,7 +7,7 @@ var server = jsonServer.create();
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(jsonServer.defaults());
 
-var router = jsonServer.router("./db.json");
+var router = jsonServer.router(path.join(__dirname, "db.json"));
 server.use(router)
 
 console.log("JSON Server: Listening on port 5000");
