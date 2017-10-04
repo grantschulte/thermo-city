473944c4d724ed392c0861c75d909df6132978a2
Grant Schulte <schulte.grant@gmail.com>
2017-10-04 07:22:25 -0400

    Adds favicons and app icons

--- src/index.js
+++ src/index.js
@@ -9,9 +9,20 @@ import "styles/app.scss";
 
 // Import Favicons
 
-// import "images/favicon.ico";
-// import "images/favicon-16x16.png";
-// import "images/favicon-32x32.png";
+import "images/favicon.ico";
+import "images/favicon-16x16.png";
+import "images/favicon-32x32.png";
+import "images/favicon-96x96.png";
+import "images/favicon-128.png";
+import "images/favicon-196x196.png";
+import "images/apple-touch-icon-57x57.png";
+import "images/apple-touch-icon-60x60.png";
+import "images/apple-touch-icon-72x72.png";
+import "images/apple-touch-icon-76x76.png";
+import "images/apple-touch-icon-114x114.png";
+import "images/apple-touch-icon-120x120.png";
+import "images/apple-touch-icon-144x144.png";
+import "images/apple-touch-icon-152x152.png";
 
 // Copy index.html to /dist
 
